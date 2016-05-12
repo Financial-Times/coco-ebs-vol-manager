0505dd91197164f829499b180042844d6ec233ca
Dan Murphy <dan.murphy@ft.com>
2016-05-12 18:05:08 +0100

    See if we can find a volume and a possible device to mount it on

--- preStart.sh
+++ preStart.sh
@@ -11,16 +11,15 @@ enviorn=`etcdctl get /ft/config/environment_tag`
 region=`etcdctl get /ft/config/aws_region`
 availZone=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 
-echo "Running prestart operation for $1 for cluster $enviorn in $region ($availZone)"
+echo "PRE-START for $1 on $enviorn in $region ($availZone)"
 
 docker pull coco/coco-ebs-vol-manager:latest
 
+volumeId=`docker run -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" -e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" coco/coco-ebs-vol-manager ./coco-ebs-vol-manager volumes find -t coco-environment-tag=${enviorn},LATEST="",store=$serviceId | jq -r '[.Volumes[0].VolumeId]'`
 
+lastDevice=$(ls -1 /dev/xvd* | sort -r | head -1 )
+lastLetter="${disk: -2}"
+nextLetter=$(echo "$lastLetter" | tr "a-z" "b-za")
+nextDrive="/dev/xvd${nextLetter}"
 
-echo "Setup persistent store for ${service} on ${instanceId}"
-
-devs = "/dev/xvdf"
-
-for i in $( ls ); do
-    echo item: $i
-done
+echo "Persistent store for ${service} on ${instanceId} may use ${volumeId} on ${nextDrive}"
