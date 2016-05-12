12bde26bd24a8aa13c088be40aee67d140e93859
Dan Murphy <dan.murphy@ft.com>
2016-05-12 15:15:27 +0100

    Prestart setting service name

--- preStart.sh
+++ preStart.sh
@@ -3,11 +3,11 @@
 # With luck running this will show how we can move persistent volumes with services.
 
 
-metadata='http://169.254.169.254/latest/meta-data/'
+
 echo "Running prestart operation for ${service} $SERVICE_ID $0 $1 $2"
 
 docker pull coco/coco-ebs-vol-manager:latest
 
-instanceId=`curl -s ${metadata}/instance-id`
+instanceId=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 
 echo "Setup persistent store for ${service} on ${instanceId}"
