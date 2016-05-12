34508fd7e6df927b6ce7b50bfd4f8843f3a91682
Dan Murphy <dan.murphy@ft.com>
2016-05-12 14:53:42 +0100

    get instanceid from metatdata

--- preStart.sh
+++ preStart.sh
@@ -4,6 +4,11 @@
 
 
 service=%p-%i
+metadata='http://169.254.169.254/latest/meta-data/'
 echo "Running prestart operation for ${service}"
 
 docker pull coco/coco-ebs-vol-manager:latest
+
+instance-id=`curl -s ${metadata}/instance-id`
+
+echo "Setup persistent store for ${service} on ${instance-id}"
