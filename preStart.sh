f8e52185819231f15deedd64895b1d17eaf0c70e
Dan Murphy <dan.murphy@ft.com>
2016-05-12 15:27:52 +0100

    Prestart getting deployment info

--- preStart.sh
+++ preStart.sh
@@ -4,10 +4,23 @@
 
 
 
-echo "Running prestart operation for ${service} $SERVICE_ID $0 $1 $2"
+
+instanceId=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+serviceId=$1
+enviorn=`etcdctl get /ft/config/environment_tag`
+region=`etcdctl get /ft/config/aws_region`
+availZone=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+
+echo "Running prestart operation for $1 for cluster $enviorn in $region ($availZone)"
 
 docker pull coco/coco-ebs-vol-manager:latest
 
-instanceId=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+
 
 echo "Setup persistent store for ${service} on ${instanceId}"
+
+devs = "/dev/xvdf"
+
+for i in $( ls ); do
+    echo item: $i
+done
