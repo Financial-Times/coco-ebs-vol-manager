05bc3cbd5713e03f04cbbe54da4eb678807ffdc4
Dan Murphy <dan.murphy@ft.com>
2016-08-23 15:34:43 +0100

    slight tweak to preStart.sh

--- preStart.sh
+++ preStart.sh
@@ -7,15 +7,21 @@ enviorn=`etcdctl get /ft/config/environment_tag`
 region=`etcdctl get /ft/config/aws_region`
 key=`etcdctl get /ft/_credentials/aws/aws_access_key_id`
 secret=`etcdctl get /ft/_credentials/aws/aws_secret_access_key`
+createVolume="true"
+volSize="5"
 instanceId=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 availZone=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 hostIP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+
+serviceTags="coco-environment-tag=${enviorn},store=$serviceId"
 
 echo "PRE-START for $serviceId on $enviorn in $region ($availZone $hostIP $instanceId)"
 
 docker pull coco/coco-ebs-vol-manager:latest
 
-volumeId=`docker run -e="AWS_ACCESS_KEY_ID=$key" -e="AWS_SECRET_ACCESS_KEY=$secret" -e="AWS_DEFAULT_REGION=$region" coco/coco-ebs-vol-manager ./coco-ebs-vol-manager -e=http://${hostIP}:2379 volumes find -t coco-environment-tag=${enviorn},LATEST="",store=$serviceId | jq -r '.Volumes[0].VolumeId'`
+volManager="docker run -e="AWS_ACCESS_KEY_ID=$key" -e="AWS_SECRET_ACCESS_KEY=$secret" -e="AWS_DEFAULT_REGION=$region" coco/coco-ebs-vol-manager ./coco-ebs-vol-manager -e=http://${hostIP}:2379"
+
+volumeId=`$volManager volumes find -t LATEST="",$serviceTags | jq -r '.Volumes[0].VolumeId'`
 
 lastDevice=$(ls -1 /dev/xvd* | sort -r | head -1 )
 lastLetter="${lastDevice: -1}"
@@ -27,4 +33,11 @@ echo "Persistent store for ${serviceId} on ${instanceId} may use ${volumeId} on 
 if [ "null" == "$volumeId" ]
 then
   echo "No volume found"
+  if [ "$createVolume" == "onNull" ]
+  then
+    echo "Creating new empty volume"
+    volumeId=`$volManager volumes create -c $volSize -a $availZone | jq -r '.Volumes[0].VolumeId'`
+    resp=`$volManager volume attach `
+    echo "Created volume $volumeId and attached it $resp"
+    resp=`$volManager tags set volumeId $serviceTags,latest=""`
 fi
